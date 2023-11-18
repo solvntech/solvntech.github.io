27f3eb0b87b0fbe95f1423b7736ef1cf53d1606c
Harry Nguyen <hainguyen27798@gmail.com>
2023-11-18 20:49:30 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["index.html"],
+    theme: {
+        extend: {
+            colors: {
+                water: "#D1D5DB",
+                midnight: "#1F2937",
+                "alice-blue": "#F9FAFB"
+            },
+        },
+    },
+    plugins: [],
+};
